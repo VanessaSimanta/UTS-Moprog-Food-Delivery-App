6cc5dfc36658d377f0a045c878efc66f3997cebe
fortunecookiees <162522300+fortunecookiees@users.noreply.github.com>
2024-09-12 20:37:21 +0700

    Update Help

--- uts_mobile_programming/lib/pages/help_Center.dart
+++ uts_mobile_programming/lib/pages/help_Center.dart
@@ -6,105 +6,192 @@ import 'package:uts_mobile_programming/pages/home.dart';
 class HelpCenter extends StatelessWidget {
   const HelpCenter({super.key});
 
- @override
+  @override
   Widget build(BuildContext context) {
+    // Mengambil ukuran layar saat ini
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Customer Service'),
-        backgroundColor: Colors.orange, // Warna AppBar
+        title: const Text('Call Us When You Need !'),
+        backgroundColor: Colors.orange,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Contact Us',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
+      body: Stack(
+        children: [
+          // Header Image Section
+          Positioned(
+            top: 0, // Menyesuaikan agar tidak menyentuh AppBar
+            left: 0,
+            right: 0,
+            height: screenHeight * 0.25,
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/header_image.jpg'), // Ganti dengan path gambar Anda
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: Center(
+                child: Text(
+                  ' Ada pertanyaan atau keluhan ? Kami siap bantu !',
+                  style: TextStyle(
+                    fontSize: screenWidth * 0.07,
+                    color: const Color.fromARGB(224, 230, 198, 125),
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
               ),
             ),
-            SizedBox(height: 16.0),
-            Text(
-              'Untuk permasalahan dan kebutuhan bisa menghubungi kami melalui kontak dibawah ini:',
-              style: TextStyle(fontSize: 16),
-            ),
-            SizedBox(height: 16.0),
-            ListTile(
-              leading: Icon(Icons.phone, color: Colors.orange),
-              title: Text('Phone'),
-              subtitle: Text('08-123-456-78'),
-              onTap: () {
-                // Implement phone call functionality
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.email, color: Colors.orange),
-              title: Text('Email'),
-              subtitle: Text('helpcenter@gmail.com'),
-              onTap: () {
-                // Implement email functionality
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.chat, color: Colors.orange),
-              title: Text('Live Chat'),
-              subtitle: Text('Klik disini untuk melakukan livechat'),
-              onTap: () {
-                // Implement live chat functionality
-              },
-            ),
-            SizedBox(height: 32.0),
-            Text(
-              'Feedback Form',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
+          ),
+          // Main Content Section
+          Positioned(
+            top: kToolbarHeight + screenHeight * 0.20, // Menyesuaikan dengan tinggi header
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: screenWidth * 0.05, // Padding disesuaikan dengan lebar layar
+                vertical: screenHeight * 0.02,  // Padding vertikal disesuaikan dengan tinggi layar
+              ),
+              child: SingleChildScrollView( // Menambahkan scroll jika konten terlalu panjang
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // Contact Us Section
+                    Text(
+                      'Contact Us',
+                      style: TextStyle(
+                        fontSize: screenWidth * 0.06, // Ukuran font disesuaikan dengan layar
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: screenHeight * 0.02), // Jarak vertikal disesuaikan
+                    Text(
+                      'Contact Us For Help:',
+                      style: TextStyle(fontSize: screenWidth * 0.04), // Ukuran teks disesuaikan
+                    ),
+                    SizedBox(height: screenHeight * 0.02),
+                    ListTile(
+                      leading: const Icon(Icons.phone, color: Colors.orange),
+                      title: const Text('Phone'),
+                      subtitle: const Text('0822-123-7768'),
+                      onTap: () {
+                        // Implement phone call functionality
+                      },
+                    ),
+                    ListTile(
+                      leading: const Icon(Icons.email, color: Colors.orange),
+                      title: const Text('Email'),
+                      subtitle: const Text('helpcenter@gmail.com'),
+                      onTap: () {
+                        // Implement email functionality
+                      },
+                    ),
+                    SizedBox(height: screenHeight * 0.04), // Jarak vertikal disesuaikan
+
+                    // Feedback Form Section
+                    Text(
+                      'Feedback Form',
+                      style: TextStyle(
+                        fontSize: screenWidth * 0.06,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: screenHeight * 0.02),
+                    Text(
+                      'Untuk kritik dan saran serta pertanyaan lebih lanjut bisa mengisi data dibawah ini. Feedback kalian sangat membantu kami untuk terus berkembang!.',
+                      style: TextStyle(fontSize: screenWidth * 0.04),
+                    ),
+                    SizedBox(height: screenHeight * 0.02),
+                    TextField(
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Your Name',
+                      ),
+                    ),
+                    SizedBox(height: screenHeight * 0.02),
+                    TextField(
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Your Email',
+                      ),
+                    ),
+                    SizedBox(height: screenHeight * 0.02),
+                    TextField(
+                      maxLines: 4,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Your Feedback',
+                      ),
+                    ),
+                    SizedBox(height: screenHeight * 0.02),
+                    ElevatedButton(
+                      onPressed: () {
+                        // Implement feedback submission functionality
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.orange, // Background color
+                        foregroundColor: Colors.white,  // Text color
+                      ),
+                      child: const Text('Submit Feedback'),
+                    ),
+                    SizedBox(height: screenHeight * 0.04),
+
+                    // FAQ Section
+                    Text(
+                      'FAQ',
+                      style: TextStyle(
+                        fontSize: screenWidth * 0.06,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: screenHeight * 0.02),
+                    ..._buildFAQ(),
+                  ],
+                ),
               ),
             ),
-            SizedBox(height: 16.0),
-            Text(
-              'Untuk kritik dan saran serta pertanyaan lebih lanjut bisa mengisi data dibawah ini. Feedback kalian sangat membantu kami untuk terus berkembang!.',
-              style: TextStyle(fontSize: 16),
-            ),
-            SizedBox(height: 16.0),
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Your Name',
-              ),
-            ),
-            SizedBox(height: 16.0),
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Your Email',
-              ),
-            ),
-            SizedBox(height: 16.0),
-            TextField(
-              maxLines: 4,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Your Feedback',
-              ),
-            ),
-            SizedBox(height: 8.0),
-          ElevatedButton(
-            onPressed: () {
-              // Implement feedback submission functionality
-              },
-                child: Text('Submit Feedback'),
-                style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.orange, // Background color
-                foregroundColor: Colors.white, // Text color
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
+
+  List<Widget> _buildFAQ() {
+    final faqList = [
+      {'question': 'How can I reset my password?', 'answer': 'To reset your password, go to the login page and click on "Forgot Password". Follow the instructions to reset your password.'},
+      {'question': 'Where can I find the user manual?', 'answer': 'The user manual can be found on our website under the "Support" section.'},
+      {'question': 'How do I contact customer support?', 'answer': 'You can contact customer support by phone at 0822-123-7768 or via email at helpcenter@gmail.com.'},
+    ];
+
+    return faqList.map((faq) {
+      return Padding(
+        padding: const EdgeInsets.only(bottom: 8.0),
+        child: ExpansionTile(
+          title: Text(
+            faq['question']!,
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(faq['answer']!),
+            ),
+          ],
+        ),
+      );
+    }).toList();
+  }
 }
 
+void main() {
+  runApp(const MaterialApp(
+    home: HelpCenter(),
+  ));
+}
