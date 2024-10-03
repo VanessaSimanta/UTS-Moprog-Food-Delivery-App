7b6d3963e524f27a2ae3a68674ca4f83f310adf5
VanessaSimanta <arleosahara@gmail.com>
2024-10-03 13:17:38 +0700

    update search
    tambah button tambah

--- uts_mobile_programming/lib/screen/home.dart
+++ uts_mobile_programming/lib/screen/home.dart
@@ -232,7 +232,7 @@ class HomePageContent extends StatelessWidget {
                             Navigator.push(
                               context,
                               MaterialPageRoute(
-                                  builder: (context) => const SearchScreen()),
+                                  builder: (context) => SearchScreen ()),
                             );
                           }
                         },
