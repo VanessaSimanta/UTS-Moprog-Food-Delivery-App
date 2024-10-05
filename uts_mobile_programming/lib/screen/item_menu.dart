5f17d2fcfa487e2c168f65380711e7c57bb18ee1
amitaoss <amita.535230019@stu.untar.ac.id>
2024-10-05 19:52:25 +0700

    add description to menu

--- uts_mobile_programming/lib/screen/item_menu.dart
+++ uts_mobile_programming/lib/screen/item_menu.dart
@@ -2,73 +2,100 @@ final List<Map<String, dynamic>> ayamItems = [
   {
     'name': 'Nasi Ayam Telor Asin',
     'price': 23000,
+    'description':
+        'Hidangan yang terdiri dari nasi putih yang disajikan bersama potongan ayam goreng renyah atau ayam yang dimasak dengan bumbu gurih. Ayam ini biasanya dilumuri dengan saus telur asin yang creamy dan gurih, terbuat dari kuning telur asin yang dihaluskan dan dicampur dengan bawang putih, margarin, serta sedikit cabai untuk memberikan sedikit rasa pedas. Tekstur sausnya lembut dan kaya rasa, melapisi ayam sehingga menciptakan perpaduan rasa gurih, asin, dan sedikit manis.',
     'rating': 4.8,
     'reviews': 30,
-    'imageURL': 'https://i.pinimg.com/originals/a9/bc/12/a9bc12c25fe6ede2fd63bdc84ffa3b1c.jpg',
+    'imageURL':
+        'https://i.pinimg.com/originals/a9/bc/12/a9bc12c25fe6ede2fd63bdc84ffa3b1c.jpg',
     'date': '2024-09-18',
   },
   {
     'name': 'Nasi Ayam Cabai Garam',
     'price': 23000,
+    'description':
+        'Hidangan yang terdiri dari nasi putih yang disajikan dengan ayam goreng yang dipotong kecil-kecil, digoreng hingga renyah, lalu dilumuri dengan bumbu cabai dan garam yang gurih dan pedas. Ayamnya digoreng bersama potongan bawang putih yang renyah, daun bawang, dan irisan cabai merah serta hijau, memberikan rasa yang kuat dan menggugah selera. Bumbu cabai garam ini memberikan sentuhan pedas yang khas dengan sedikit sensasi asin yang pas.',
     'rating': 4.7,
     'reviews': 25,
-    'imageURL': 'https://i.pinimg.com/564x/c3/a3/fa/c3a3faaee5d92ad7d77eaaf1b6a14a22.jpg',
+    'imageURL':
+        'https://i.pinimg.com/564x/c3/a3/fa/c3a3faaee5d92ad7d77eaaf1b6a14a22.jpg',
     'date': '2024-02-05',
   },
   {
     'name': 'Nasi Ayam Kremes',
     'price': 23000,
+    'description':
+        'Hidangan yang terdiri dari nasi putih yang disajikan dengan ayam goreng yang dilapisi dengan kremesan renyah dan gurih. Ayamnya biasanya dimarinasi dengan bumbu khas seperti bawang putih, ketumbar, kunyit, dan garam, kemudian digoreng hingga berwarna keemasan. Bagian istimewa dari hidangan ini adalah kremesannya, yaitu adonan tepung berbumbu yang digoreng hingga renyah, memberikan tekstur yang kontras dengan daging ayam yang lembut.',
     'rating': 4.6,
     'reviews': 14,
-    'imageURL': 'https://i.pinimg.com/564x/fe/c3/62/fec3627605cce6567a5526585f09d4ef.jpg',
+    'imageURL':
+        'https://i.pinimg.com/564x/fe/c3/62/fec3627605cce6567a5526585f09d4ef.jpg',
     'date': '2024-02-05',
   },
   {
     'name': 'Nasi Ayam Bakar',
     'price': 23000,
+    'descripton':
+        'Hidangan yang terdiri dari nasi putih yang disajikan dengan ayam yang dipanggang atau dibakar menggunakan bumbu khas. Ayamnya terlebih dahulu dimarinasi dengan campuran bumbu seperti kecap manis, bawang putih, bawang merah, ketumbar, dan rempah-rempah lainnya, lalu dipanggang hingga matang sempurna dan menghasilkan aroma bakar yang harum serta rasa manis dan gurih.',
     'rating': 4.6,
     'reviews': 14,
-    'imageURL': 'https://i.pinimg.com/564x/97/56/e7/9756e7ea800f54248683c0ee40b0f3e3.jpg',
+    'imageURL':
+        'https://i.pinimg.com/564x/97/56/e7/9756e7ea800f54248683c0ee40b0f3e3.jpg',
     'date': '2023-09-28',
   },
   {
     'name': 'Nasi Ayam Hainan',
     'price': 23000,
+    'description':
+        'Hidangan khas dari Hainan, China, yang sangat populer di banyak negara Asia Tenggara, terutama Singapura dan Malaysia. Hidangan ini terdiri dari ayam yang dimasak dengan teknik poaching, yang direbus perlahan dalam kaldu aromatik hingga dagingnya empuk dan beraroma lembut. Ayam Hainan biasanya disajikan dengan kulit yang halus dan daging yang juicy, terkadang dengan sedikit minyak ayam di atasnya untuk menambah rasa.',
     'rating': 4.3,
     'reviews': 8,
-    'imageURL': 'https://i.pinimg.com/736x/4e/80/74/4e80740f4c9bfc01c5e0c26886a975cb.jpg',
+    'imageURL':
+        'https://i.pinimg.com/736x/4e/80/74/4e80740f4c9bfc01c5e0c26886a975cb.jpg',
     'date': '2024-10-18',
   },
   {
     'name': 'Nasi Ayam Lada Hitam',
     'price': 23000,
+    'descirption':
+        'Hidangan lezat yang terdiri dari nasi putih yang disajikan dengan potongan ayam yang dimasak menggunakan saus lada hitam yang gurih dan pedas. Ayamnya biasanya dipotong dadu atau diiris tipis, kemudian ditumis dengan bawang bombay, paprika, dan saus lada hitam. Sausnya terbuat dari campuran kecap, lada hitam yang dihaluskan, bawang putih, serta sedikit gula dan garam, menciptakan cita rasa pedas, gurih, dan sedikit manis.',
     'rating': 4.5,
     'reviews': 13,
-    'imageURL': 'https://i.pinimg.com/564x/9a/17/be/9a17be400d3c37118f73c71ea4b8e42b.jpg',
+    'imageURL':
+        'https://i.pinimg.com/564x/9a/17/be/9a17be400d3c37118f73c71ea4b8e42b.jpg',
     'date': '2024-02-05',
   },
   {
     'name': 'Nasi Ayam Kungpau',
     'price': 21000,
+    'description':
+        'Hidangan khas Tiongkok yang terdiri dari nasi putih yang disajikan dengan potongan ayam yang ditumis dalam saus pedas manis khas Kungpao. Ayam biasanya dipotong dadu dan dimasak bersama kacang tanah, cabai kering, bawang putih, serta jahe. Saus Kungpao terbuat dari campuran kecap asin, cuka, gula, dan sedikit minyak wijen, memberikan rasa yang seimbang antara manis, gurih, dan pedas.',
     'rating': 4.2,
     'reviews': 6,
-    'imageURL': 'https://i.pinimg.com/564x/54/43/38/54433839d0dbc19d8ece1cf2a045e7f7.jpg',
+    'imageURL':
+        'https://i.pinimg.com/564x/54/43/38/54433839d0dbc19d8ece1cf2a045e7f7.jpg',
     'date': '2024-11-07',
   },
   {
     'name': 'Nasi Ayam Sambal Matah',
     'price': 20000,
+    'description':
+        'Hidangan khas Indonesia, khususnya dari Bali, yang terdiri dari nasi putih hangat yang disajikan dengan ayam goreng atau ayam panggang, serta sambal matah sebagai pelengkap utama. Sambal matah adalah sambal mentah yang terbuat dari irisan bawang merah, cabai rawit, serai, daun jeruk, dan sedikit perasan jeruk limau, kemudian ditumis sebentar dengan minyak kelapa panas. Sambal ini memberikan rasa segar, pedas, dan sedikit asam yang khas.',
     'rating': 4.2,
     'reviews': 4,
-    'imageURL': 'https://i.pinimg.com/564x/7c/02/79/7c02792d0b235e9b685a73e4ed0ac161.jpg',
+    'imageURL':
+        'https://i.pinimg.com/564x/7c/02/79/7c02792d0b235e9b685a73e4ed0ac161.jpg',
     'date': '2024-02-05',
   },
   {
     'name': 'Nasi Ayam Teriyaki',
     'price': 23000,
+    'description':
+        'Hidangan yang terdiri dari nasi putih hangat yang disajikan dengan potongan ayam yang dimasak menggunakan saus teriyaki yang manis dan gurih. Ayamnya biasanya dipanggang atau ditumis hingga matang sempurna, lalu dilumuri dengan saus teriyaki khas Jepang yang terbuat dari campuran kecap asin, gula, dan mirin, menghasilkan rasa manis yang lezat dengan sedikit sentuhan asin.',
     'rating': 4.8,
     'reviews': 18,
-    'imageURL': 'https://i.pinimg.com/564x/ef/04/93/ef049345207584226bd52887e37fa448.jpg',
+    'imageURL':
+        'https://i.pinimg.com/564x/ef/04/93/ef049345207584226bd52887e37fa448.jpg',
     'date': '2024-06-08',
   },
 ];
@@ -77,57 +104,78 @@ final List<Map<String, dynamic>> seafoodItems = [
   {
     'name': 'Nasi Udang Cabai Garam',
     'price': 26000,
+    'description':
+        'Hidangan yang terdiri dari nasi putih hangat yang disajikan dengan udang goreng renyah yang dilumuri bumbu cabai garam yang pedas dan gurih. Udang biasanya digoreng hingga berwarna keemasan, menjaga teksturnya yang renyah di luar namun tetap juicy di dalam. Setelah digoreng, udang ditumis dengan bawang putih cincang, cabai merah dan hijau, serta taburan garam yang menciptakan rasa pedas dan asin yang kuat namun seimbang.',
     'rating': 4.6,
     'reviews': 12,
-    'imageURL': 'https://i.pinimg.com/564x/5f/f3/be/5ff3be89f4fe42a185b1917e2ea5a055.jpg',
+    'imageURL':
+        'https://i.pinimg.com/564x/5f/f3/be/5ff3be89f4fe42a185b1917e2ea5a055.jpg',
     'date': '2024-10-05',
   },
   {
     'name': 'Nasi Goreng Udang',
     'price': 26000,
+    'description':
+        'Hidangan populer yang terdiri dari nasi yang digoreng bersama udang, bumbu, dan berbagai sayuran. Nasi yang digunakan biasanya adalah nasi putih yang sudah dingin, sehingga lebih mudah untuk digoreng dan tidak lengket.',
     'rating': 4.4,
     'reviews': 10,
-    'imageURL': 'https://i.pinimg.com/564x/43/69/8b/43698b305f75bd8adf5761ba44d62d1c.jpg',
+    'imageURL':
+        'https://i.pinimg.com/564x/43/69/8b/43698b305f75bd8adf5761ba44d62d1c.jpg',
     'date': '2024-10-11',
   },
   {
     'name': 'Nasi Udang Saos Balado',
     'price': 26000,
+    'description':
+        'Hidangan khas Indonesia yang terdiri dari nasi putih yang disajikan dengan udang yang dimasak dalam saus balado yang pedas dan menggugah selera. Saus balado terbuat dari campuran cabai merah, bawang merah, bawang putih, dan rempah-rempah lainnya yang ditumis hingga harum dan matang. Saus ini memberikan rasa pedas yang khas, serta sedikit manis dan asin yang seimbang.',
     'rating': 4.3,
     'reviews': 8,
-    'imageURL': 'https://i.pinimg.com/564x/07/b1/43/07b1437f922f51e7505bbe639d3797ed.jpg',
+    'imageURL':
+        'https://i.pinimg.com/564x/07/b1/43/07b1437f922f51e7505bbe639d3797ed.jpg',
     'date': '2024-12-08',
   },
   {
     'name': 'Nasi Cumi Sambal Matah',
     'price': 26000,
+    'description':
+        'Hidangan khas Indonesia, terutama dari Bali, yang menggabungkan nasi putih hangat dengan cumi-cumi yang dimasak dan sambal matah yang segar.',
     'rating': 4.5,
     'reviews': 14,
-    'imageURL': 'https://i.pinimg.com/564x/a9/5b/c9/a95bc9761ed960c2cc4d118e7f52255d.jpg',
+    'imageURL':
+        'https://i.pinimg.com/564x/a9/5b/c9/a95bc9761ed960c2cc4d118e7f52255d.jpg',
     'date': '2024-02-05',
   },
   {
     'name': 'Nasi Cumi Goreng Tepung',
     'price': 26000,
+    'description':
+        'Hidangan yang terdiri dari nasi putih yang disajikan dengan cumi-cumi yang dilapisi tepung dan digoreng hingga renyah.',
     'rating': 4.7,
     'reviews': 18,
-    'imageURL': 'https://i.pinimg.com/564x/f6/4d/d4/f64dd4b73741eafa8ce4e1637a9592c0.jpg',
+    'imageURL':
+        'https://i.pinimg.com/564x/f6/4d/d4/f64dd4b73741eafa8ce4e1637a9592c0.jpg',
     'date': '2024-03-05',
   },
   {
     'name': 'Nasi Cumi Saos Padang',
     'price': 26000,
+    'description':
+        'Hidangan khas Indonesia yang kaya akan rasa pedas dan aromatik, terbuat dari nasi putih yang disajikan dengan cumi-cumi yang dimasak dalam saus Padang yang khas',
     'rating': 4.6,
     'reviews': 16,
-    'imageURL': 'https://i.pinimg.com/564x/f6/4d/d4/f64dd4b73741eafa8ce4e1637a9592c0.jpg',
+    'imageURL':
+        'https://i.pinimg.com/564x/f6/4d/d4/f64dd4b73741eafa8ce4e1637a9592c0.jpg',
     'date': '2024-02-06',
   },
   {
     'name': 'Nasi Cumi Asam Manis',
     'price': 26000,
+    'description':
+        'Hidangan yang terdiri dari nasi putih yang disajikan dengan cumi-cumi yang dimasak dalam saus asam manis yang lezat.',
     'rating': 4.4,
     'reviews': 11,
-    'imageURL': 'https://i.pinimg.com/564x/0e/63/87/0e63871b71163c4e5da3125d937dfd7b.jpg',
+    'imageURL':
+        'https://i.pinimg.com/564x/0e/63/87/0e63871b71163c4e5da3125d937dfd7b.jpg',
     'date': '2024-07-05',
   },
 ];
@@ -136,17 +184,23 @@ final List<Map<String, dynamic>> healthItems = [
   {
     'name': 'Nasi FuyungHai',
     'price': 19800,
+    'description':
+        'Hidangan khas Tionghoa yang populer di Indonesia, terdiri dari nasi putih yang disajikan dengan fuyunghai, yaitu omelet telur yang diisi dengan berbagai bahan seperti daging (sering kali daging ayam atau udang), sayuran, dan bumbu',
     'rating': 4.4,
     'reviews': 12,
-    'imageURL': 'https://i.pinimg.com/564x/ef/a2/3d/efa23dca770ade3382717ce622f253ac.jpg',
+    'imageURL':
+        'https://i.pinimg.com/564x/ef/a2/3d/efa23dca770ade3382717ce622f253ac.jpg',
     'date': '2024-12-05',
   },
   {
     'name': 'Nasi Chapcai',
     'price': 19800,
+    'description':
+        'Hidangan yang terdiri dari nasi putih yang disajikan dengan tumisan sayuran dan protein, biasanya menggunakan bahan-bahan yang beragam seperti ayam, udang, atau tofu, yang dimasak dalam gaya masakan Tionghoa.',
     'rating': 4.3,
     'reviews': 11,
-    'imageURL': 'https://i.pinimg.com/564x/e7/14/0d/e7140d865438d72045fc67cc3b292d2b.jpg',
+    'imageURL':
+        'https://i.pinimg.com/564x/e7/14/0d/e7140d865438d72045fc67cc3b292d2b.jpg',
     'date': '2024-02-10',
   },
 ];
@@ -155,136 +209,171 @@ final List<Map<String, dynamic>> minumanItems = [
   {
     'name': 'Es Teh Manis',
     'price': 5000,
+    'description':
+        'Teh yang diseduh dan ditambahkan dengan gula, kemudian disajikan dalam keadaan dingin dengan es batu',
     'rating': 5.0,
     'reviews': 37,
-    'imageURL': 'https://i.pinimg.com/564x/0a/7c/27/0a7c27f56aa886c3451e4cf97bc37e57.jpg',
+    'imageURL':
+        'https://i.pinimg.com/564x/0a/7c/27/0a7c27f56aa886c3451e4cf97bc37e57.jpg',
     'date': '2024-02-05',
   },
   {
     'name': 'Es Teh Tawar',
     'price': 3000,
+    'description':
+        'Teh yang diseduh dan disajikan dalam keadaan dingin tanpa tambahan gula',
     'rating': 4.3,
     'reviews': 11,
-    'imageURL': 'https://i.pinimg.com/564x/70/d9/5e/70d95e4a4ff2df1b2d4b100a2d8b3f28.jpg',
+    'imageURL':
+        'https://i.pinimg.com/564x/70/d9/5e/70d95e4a4ff2df1b2d4b100a2d8b3f28.jpg',
     'date': '2024-03-05',
   },
   {
     'name': 'Lemon Tea',
     'price': 12000,
+    'description':
+        'Teh yang diseduh dan ditambahkan perasan lemon, menciptakan kombinasi rasa manis dan asam,',
     'rating': 4.8,
     'reviews': 18,
-    'imageURL': 'https://i.pinimg.com/564x/96/ee/d9/96eed9822024adfcf31109821b87ab2d.jpg',
+    'imageURL':
+        'https://i.pinimg.com/564x/96/ee/d9/96eed9822024adfcf31109821b87ab2d.jpg',
     'date': '2024-08-05',
   },
   {
-      'name': 'Le Minerale',
-      'price': 3000,
-      'rating': 4.3,
-      'reviews': 11,
-      'imageURL':
-          'https://i.pinimg.com/564x/f7/15/62/f71562e1099d6d8a7fa5ba1bcfb6f587.jpg',
-      'date': '2024-02-05',
-    },
-    {
-      'name': ' Es Jeruk Besar',
-      'price': 11000,
-      'rating': 4.8,
-      'reviews': 23,
-      'imageURL':
-          'https://i.pinimg.com/564x/2f/ce/05/2fce054dc77697bbc790197ed8c71bbf.jpg',
-      'date': '2024-02-05',
-    },
-    {
-      'name': ' Es Jeruk Nipis',
-      'price': 8000,
-      'rating': 4.3,
-      'reviews': 6,
-      'imageURL':
-          'https://i.pinimg.com/564x/e5/c8/2c/e5c82c09694828975a4dc3bae3f05bae.jpg',
-      'date': '2024-02-05',
-    },
-    {
-      'name': 'Teh Botol',
-      'price': 6000,
-      'rating': 4.7,
-      'reviews': 16,
-      'imageURL':
-          'https://i.pinimg.com/564x/ef/a7/27/efa727a3e6cd732e81084406ea7e7e71.jpg',
-      'date': '2024-02-05',
-    },
+    'name': 'Le Minerale',
+    'price': 3000,
+    'description':
+        'Merek air mineral yang terkenal di Indonesia yang berasal dari sumber mata air alami, yang telah melalui proses penyaringan untuk memastikan kebersihan dan keamanannya',
+    'rating': 4.3,
+    'reviews': 11,
+    'imageURL':
+        'https://i.pinimg.com/564x/f7/15/62/f71562e1099d6d8a7fa5ba1bcfb6f587.jpg',
+    'date': '2024-02-05',
+  },
+  {
+    'name': ' Es Jeruk Besar',
+    'price': 11000,
+    'description':
+        'Minuman segar yang terbuat dari perasan jeruk segar yang dicampur dengan air dan es, dengan kombinasi rasa manis dan asam yang menyegarkan di hari yang panas.',
+    'rating': 4.8,
+    'reviews': 23,
+    'imageURL':
+        'https://i.pinimg.com/564x/2f/ce/05/2fce054dc77697bbc790197ed8c71bbf.jpg',
+    'date': '2024-02-05',
+  },
+  {
+    'name': ' Es Jeruk Nipis',
+    'price': 8000,
+    'description':
+        'Minuman segar yang terbuat dari perasan jeruk nipis yang dicampur dengan air dan es batu, dengan rasa asamnya yang menyegarkan dan cocok untuk menghilangkan dahaga',
+    'rating': 4.3,
+    'reviews': 6,
+    'imageURL':
+        'https://i.pinimg.com/564x/e5/c8/2c/e5c82c09694828975a4dc3bae3f05bae.jpg',
+    'date': '2024-02-05',
+  },
+  {
+    'name': 'Teh Botol',
+    'price': 6000,
+    'description':
+        'Minuman siap saji yang terbuat dari teh yang telah diseduh dan dibotolkan untuk kenyamanan konsumen',
+    'rating': 4.7,
+    'reviews': 16,
+    'imageURL':
+        'https://i.pinimg.com/564x/ef/a7/27/efa727a3e6cd732e81084406ea7e7e71.jpg',
+    'date': '2024-02-05',
+  },
 ];
 
 final List<Map<String, dynamic>> ayamAlacarteItems = [
   {
     'name': 'Ayam Telor Asin',
     'price': 30000,
+    'description': 'Ayam telor asin tanpa nasi',
     'rating': 4.0,
     'reviews': 15,
-    'imageURL': 'https://i.pinimg.com/564x/a3/6b/86/a36b86e08edaf2fe34bcbe65b722e9f8.jpg',
+    'imageURL':
+        'https://i.pinimg.com/564x/a3/6b/86/a36b86e08edaf2fe34bcbe65b722e9f8.jpg',
     'date': '2024-09-18',
   },
   {
     'name': 'Ayam Cabai Garam',
     'price': 28000,
+    'description': 'Ayam cabai garam tanpa nasi',
     'rating': 4.3,
     'reviews': 25,
-    'imageURL': 'https://i.pinimg.com/564x/f9/02/2a/f9022ada4a6b63c60a09a912da20a4a0.jpg',
+    'imageURL':
+        'https://i.pinimg.com/564x/f9/02/2a/f9022ada4a6b63c60a09a912da20a4a0.jpg',
     'date': '2024-02-05',
   },
   {
     'name': 'Ayam Kremes',
     'price': 27000,
+    'description': 'Ayam kremes tanpa nasi',
     'rating': 4.6,
     'reviews': 10,
-    'imageURL': 'https://i.pinimg.com/564x/ec/7a/74/ec7a74bbca9214f8b3022ab90006d411.jpg',
+    'imageURL':
+        'https://i.pinimg.com/564x/ec/7a/74/ec7a74bbca9214f8b3022ab90006d411.jpg',
     'date': '2024-02-05',
   },
   {
     'name': 'Ayam Bakar',
     'price': 25000,
+    'description': 'Ayam bakar tanpa nasi',
     'rating': 4.0,
     'reviews': 14,
-    'imageURL': 'https://i.pinimg.com/736x/80/3c/bc/803cbcabba9e4f43b52ea660ac726b90.jpg',
+    'imageURL':
+        'https://i.pinimg.com/736x/80/3c/bc/803cbcabba9e4f43b52ea660ac726b90.jpg',
     'date': '2023-09-28',
   },
   {
     'name': 'Ayam Hainan',
     'price': 23000,
+    'description': 'Ayam Hainan tanpa nasi',
     'rating': 4.3,
     'reviews': 8,
-    'imageURL': 'https://i.pinimg.com/564x/06/91/f8/0691f86ab4725c59b33914b95599a399.jpg',
+    'imageURL':
+        'https://i.pinimg.com/564x/06/91/f8/0691f86ab4725c59b33914b95599a399.jpg',
     'date': '2024-10-18',
   },
   {
     'name': 'Ayam Lada Hitam',
     'price': 27000,
+    'description': 'Ayam lada hitam tanpa nasi',
     'rating': 4.5,
     'reviews': 13,
-    'imageURL': 'https://i.pinimg.com/564x/2c/44/98/2c44980f64dabb6651ea96c0fd3e1754.jpg',
+    'imageURL':
+        'https://i.pinimg.com/564x/2c/44/98/2c44980f64dabb6651ea96c0fd3e1754.jpg',
     'date': '2024-02-05',
   },
   {
     'name': 'Ayam Kungpau',
     'price': 21000,
+    'description': 'Ayam Kungpao tanpa nasi',
     'rating': 4.1,
     'reviews': 6,
-    'imageURL': 'https://i.pinimg.com/564x/d7/cd/e2/d7cde2835fc673e4d6b7f823b7314098.jpg',
+    'imageURL':
+        'https://i.pinimg.com/564x/d7/cd/e2/d7cde2835fc673e4d6b7f823b7314098.jpg',
     'date': '2024-11-07',
   },
   {
     'name': 'Ayam Sambal Matah',
     'price': 24000,
+    'description': 'Ayam sambal matah tanpa nasi',
     'rating': 4.2,
     'reviews': 4,
-    'imageURL': 'https://i.pinimg.com/564x/c9/01/a0/c901a002f37b719a6b1b1ed6ecf1451a.jpg',
+    'imageURL':
+        'https://i.pinimg.com/564x/c9/01/a0/c901a002f37b719a6b1b1ed6ecf1451a.jpg',
     'date': '2024-02-05',
   },
   {
     'name': 'Ayam Teriyaki',
     'price': 24000,
+    'description': 'Ayam teriyaki tanpa nasi',
     'rating': 4.8,
     'reviews': 18,
-    'imageURL': 'https://i.pinimg.com/564x/ef/27/10/ef27109b494c335f4a1b4aa6af286dc8.jpg',
+    'imageURL':
+        'https://i.pinimg.com/564x/ef/27/10/ef27109b494c335f4a1b4aa6af286dc8.jpg',
     'date': '2024-06-08',
   },
 ];
